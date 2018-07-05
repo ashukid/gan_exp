8a8cca2079ff1973d620cb156cd3e95abe910b16
root <irm2015001@iiita.ac.in>
2018-07-05 12:01:25 +0000

    add to architecture

--- tfgan/.ipynb_checkpoints/architectures-checkpoint.py
+++ tfgan/.ipynb_checkpoints/architectures-checkpoint.py
@@ -7,57 +7,91 @@ from ops import *
 # DCGAN architecture
 class DCGAN:
     
-    def __init__(self,image_height,image_width,image_channel,output_height,output_width,output_channel,z_shape):
-        self.image_height = image_height
-        self.image_width = image_width
+    def __init__(self,image_size,image_channel,output_size,output_channel,z_shape):
+        self.image_size = image_size
         self.image_channel = image_channel
-        self.output_height = output_height
-        self.output_width = output_width
+        self.output_size = output_height
         self.output_channel = output_channel
         self.z_shape = z_shape
     
+    
+    def Discriminator(self,image,reuse=False,name='discriminator'):
+        
+        
+        maxpool_stride = 2
+        normal_stride = 1
+        df_size = 5 # dsicriminator filter
+        df_channel = 32
+
+        image = tf.reshape(image, [-1,self.image_size,self.image_size,self.image_channel])
+        
+        with tf.variable_scope(name,reuse=reuse):
+        
+            l1 = lrelu(conv2d(X,df_size,self.image_channel,df_channel,maxpool_stride,'l1'))
+            l2 = lrelu(conv2d(l1,df_size,df_channel,df_channel*2,maxpool_stride,'l2'))
+            l3 = lrelu(conv2d(l2,df_size,df_channel*2,df_channel*4,maxpool_stride,'l3'))
+            l4 = lrelu(conv2d(l3,l3.get_shape()[1],df_channel*4,1024,normal_stride,'l4','valid')) # valid padding
+            l5 = conv2d(l4,1,1024,1,normal_stride,'l5','valid') # valid padding
+            output = tf.reshape(l5,shape=[-1,1*1*1])
+        
+        return output
     
     def Discriminator_bn(self,image,reuse=False,name='discriminator'):
         
         
         maxpool_stride = 2
         normal_stride = 1
-        filter_channel = 32
-        image = tf.reshape(image, [-1,self.image_height,self.image_width,self.image_channel])
+        df_size = 5 # dsicriminator filter
+        df_channel = 32
+
+        image = tf.reshape(image, [-1,self.image_size,self.image_size,self.image_channel])
         
         with tf.variable_scope(name,reuse=reuse):
-        
-            # no batch norm to discriminator input layer
-            layer1 = lrelu(conv2d(image,5,image.get_shape()[-1].value,filter_channel,maxpool_stride))
-            layer2 = lrelu(batch_norm(conv2d(layer1,5,filter_channel,filter_channel*2,maxpool_stride)))
-            layer3 = lrelu(batch_norm(conv2d(layer2,5,filter_channel*2,filter_channel*4,maxpool_stride)))
-            layer4 = lrelu(batch_norm(conv2d(layer3,self.image_height//8,filter_channel*4,1024,normal_stride)))
-            output = lrelu(batch_norm(conv2d(layer4,1,1024,1,normal_stride)))
-            output = tf.reshape(output,shape=(-1,1*1*1))
+            l1 = lrelu(conv2d(X,df_size,self.image_channel,df_channel,maxpool_stride,'l1'))
+            l2 = lrelu(batch_norm(conv2d(l1,df_size,df_channel,df_channel*2,maxpool_stride,'l2')))
+            l3 = lrelu(batch_norm(conv2d(l2,df_size,df_channel*2,df_channel*4,maxpool_stride,'l3')))
+            l4 = lrelu(batch_norm(conv2d(l3,l3.get_shape()[1],df_channel*4,1024,normal_stride,'l4','valid'))) # valid padding
+            l5 = conv2d(l4,1,1024,1,normal_stride,'l5','valid') # valid padding
+            output = tf.reshape(l5,shape=[-1,1*1*1])
 
-            return output,sigmoid(output)
+        return output
+           
         
     
-    def Generator_bn(self,z,reuse=False,name='generator'):
+    def Generator(self,z,reuse=False,name='generator'):
         
-        assert self.output_height == self.output_width
-        gf = int(self.output_height//16)
-        stride = 2
-        filter_shape = 5
+        
+        gf_size = image_size //16 # generator filter
+        gf_channel = 40
         
         with tf.variable_scope(name,reuse=reuse):
-        
-            # no batch norm to generator output layer
-            layer1 = linear(z,gf*gf*5*8)
-            layer1 = tf.reshape(layer1,shape=(-1,gf,gf,filter_shape*8))
-            layer1 = relu(batch_norm(layer1))
-            layer2 = relu(batch_norm(deconv2d(layer1,filter_shape*8,filter_shape*4,stride)))
-            layer3 = relu(batch_norm(deconv2d(layer2,filter_shape*4,filter_shape*2,stride)))
-            layer4 = relu(batch_norm(deconv2d(layer3,filter_shape*2,filter_shape,stride)))
-            layer5 = tanh(deconv2d(layer4,filter_shape,self.output_channel,stride))
+            l1 = relu(linear(z,gf_size*gf_size*gf_channel,'l1'))
+            l1 = tf.reshape(l1,[-1,gf_size,gf_size,gf_channel])
+            l2 = relu(deconv2d(l1,gf_channel,gf_channel//2,2,'l2'))
+            l3 = relu(deconv2d(l2,gf_channel//2,gf_channel//4,2,'l3'))        
+            l4 = relu(deconv2d(l3,gf_channel//4,gf_channel//8,2,'l4'))  
+            l5 = tanh(deconv2d(l4,gf_channel//8,image_channel,2,'l5'))
 
-            return layer5
+            return l5
+        
+    def Generator_bn(self,z,reuse=False,name='generator'):
+        
+        
+        gf_size = image_size //16 # generator filter
+        gf_channel = 40
+        
+        with tf.variable_scope(name,reuse=reuse):
+            l1 = relu(batch_norm(linear(z,gf_size*gf_size*gf_channel,'l1')))
+            l1 = tf.reshape(l1,[-1,gf_size,gf_size,gf_channel])
+            l2 = relu(batch_norm(deconv2d(l1,gf_channel,gf_channel//2,2,'l2')))
+            l3 = relu(batch_norm(deconv2d(l2,gf_channel//2,gf_channel//4,2,'l3')))        
+            l4 = relu(batch_norm(deconv2d(l3,gf_channel//4,gf_channel//8,2,'l4')))  
+            l5 = tanh(deconv2d(l4,gf_channel//8,image_channel,2,'l5'))
+
+            return l5
+    
         
 #####################################################################################################################
 # todo 
 # other architectures
+# like resnet
--- tfgan/.ipynb_checkpoints/ops-checkpoint.py
+++ tfgan/.ipynb_checkpoints/ops-checkpoint.py
@@ -1,19 +1,21 @@
 import tensorflow as tf
 import numpy as np
 
-def conv2d(x,filter_size,in_channel,out_channel,stride,name):
+def conv2d(x,filter_size,in_channel,out_channel,stride,name,padding='same'):
     # default filter size (5,5)
     with tf.variable_scope(name):
-        w = tf.get_variable(name='kernel',shape=(filter_size,filter_size,in_channel,out_channel),initializer=tf.contrib.layers.xavier_initializer())
-        b = tf.get_variable(name='biases',shape=(out_channel,),initializer=tf.constant_initializer())
-        layer = tf.nn.conv2d(x,filter=w,strides=[1,stride,stride,1],padding='SAME')+b
+        w = tf.get_variable(name='kernel',shape=[filter_size,filter_size,in_channel,out_channel],
+                            initializer=tf.contrib.layers.xavier_initializer())
+        b = tf.get_variable(name='biases',shape=[out_channel],initializer=tf.constant_initializer())
+        layer = tf.nn.conv2d(x,filter=w,strides=[1,stride,stride,1],padding=padding.upper())+b
     
     return layer
 
 def linear(x,out_neuron,name):
     with tf.variable_scope(name):
-        w = tf.get_variable(name='weights',shape=(x.get_shape()[1].value,out_neuron),initializer=tf.contrib.layers.xavier_initializer())
-        b = tf.get_variable(name='biases',shape=(out_neuron,),initializer=tf.constant_initializer())
+        w = tf.get_variable(name='weights',shape=[x.get_shape()[1].value,out_neuron],
+                            initializer=tf.contrib.layers.xavier_initializer())
+        b = tf.get_variable(name='biases',shape=[out_neuron],initializer=tf.constant_initializer())
         layer = tf.matmul(x,w)+b
     return layer
 
@@ -37,14 +39,21 @@ def tanh(x):
     return tf.nn.tanh(x)
 
 def batch_norm(x):
-    layer = tf.layers.batch_normalization(x)
+    layer = tf.contrib.layers.batch_norm(x)
     return layer
 
-def deconv2d(x,in_channel,out_channel,stride):
-    w = tf.Variable(tf.random_normal(shape=(5,5,out_channel,in_channel),stddev=0.02))
-    out_w = x.get_shape()[1].value*stride
-    out_h = x.get_shape()[2].value*stride
-    out_shape = [x.get_shape()[0].value,out_w,out_h,out_channel]
-    out_shape = tf.stack(out_shape)
-    layer = tf.nn.conv2d_transpose(x,filter=w,output_shape=out_shape,strides=[1,stride,stride,1],padding='SAME')
-    return layer
+def deconv2d(x,in_channel,out_channel,stride,name,):
+    with tf.variable_scope(name):
+        w = tf.get_variable(name='kernel',shape=[5,5,out_channel,in_channel],
+                            initializer=tf.contrib.layers.xavier_initializer())
+        out_w = x.get_shape()[1].value*stride
+        out_h = x.get_shape()[2].value*stride
+        if(x.get_shape()[0] == None):
+            print("Placeholder Error : Define placeholder with batch_size instead of 'None' ")
+            return None
+        # outshape needs exact integers instead on None or -1
+        out_shape = [x.get_shape()[0].value,out_w,out_h,out_channel]
+        out_shape = tf.stack(out_shape)
+        layer = tf.nn.conv2d_transpose(x,filter=w,output_shape=out_shape,
+                                       strides=[1,stride,stride,1],padding='SAME')
+        return layer
